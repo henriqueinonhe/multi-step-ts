4861a34e33c468176f710f74940bdaf6b47320c2
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2023-05-30 12:03:46 -0300

    Implement base logic

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import { Override } from "./utils";
-
 export type TicketFlow =
   | TicketFlowAtPickingMovie
   | TicketFlowAtPickingMovieDate
@@ -9,8 +7,6 @@ export type TicketFlow =
 export type Movie = {
   id: string;
   name: string;
-  pricePerTicket: number;
-  date: string;
 };
 
 export type Seat = {
@@ -24,23 +20,21 @@ export type TicketFlowAtPickingMovie = {
 
 export type TicketFlowAtPickingMovieDate = {
   step: typeof TicketFlowSteps.PickingMovie.PickingDate;
-  movie: Override<
-    Movie,
-    {
-      date?: undefined;
-      pricePerTicket?: undefined;
-    }
-  >;
+  movie: Movie;
 };
 
 export type TicketFlowAtSelectingSeats = {
   step: typeof TicketFlowSteps.SelectingSeats;
   movie: Movie;
+  pricePerTicket: number;
+  date: string;
 };
 
 export type TicketFlowAtCheckout = {
   step: typeof TicketFlowSteps.Checkout;
   movie: Movie;
+  pricePerTicket: number;
+  date: string;
   seats: Array<Seat>;
 };
 
@@ -52,3 +46,52 @@ export const TicketFlowSteps = {
   SelectingSeats: "SelectingSeats",
   Checkout: "Checkout",
 } as const;
+
+export const createInitialTicketFlow = (): TicketFlowAtPickingMovie => ({
+  step: TicketFlowSteps.PickingMovie.PickingMovie,
+});
+
+type PickMovieParameters = {
+  movie: Movie;
+};
+
+export const pickMovie = (
+  ticketFlow: TicketFlowAtPickingMovie,
+  { movie }: PickMovieParameters
+): TicketFlowAtPickingMovieDate => ({
+  ...ticketFlow,
+  step: TicketFlowSteps.PickingMovie.PickingDate,
+  movie,
+});
+
+type PickDateParameters = {
+  date: string;
+  pricePerTicket: number;
+};
+
+export const pickDate = (
+  ticketFlow: TicketFlowAtPickingMovieDate,
+  { date, pricePerTicket }: PickDateParameters
+): TicketFlowAtSelectingSeats => ({
+  ...ticketFlow,
+  step: TicketFlowSteps.SelectingSeats,
+  date,
+  pricePerTicket,
+});
+
+type PickSeatsParameters = {
+  seats: Array<Seat>;
+};
+
+export const pickSeats = (
+  ticketFlow: TicketFlowAtSelectingSeats,
+  { seats }: PickSeatsParameters
+): TicketFlowAtCheckout => ({
+  ...ticketFlow,
+  step: TicketFlowSteps.Checkout,
+  seats,
+});
+
+export const checkout = async (ticketFlow: TicketFlow) => {
+  return createInitialTicketFlow();
+};
